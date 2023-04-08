cb5be41044bbe8eca83cabbbac5110b03823d511
vheran <vheran@student.42lausanne.ch>
2023-04-09 00:37:37 +0200

    Upload by leroierrant 2023-04-09_00:37:37

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -9,5 +9,63 @@
 # include <deque>
 # include <stdexcept>
 # include <exception>
+# include <time.h>
+# include <type_traits>
+
+# define INSERT_THRESHOLD 64
+
+template<typename Container>
+Container	fillContainer(const std::string& input) {
+	Container			result;
+	std::istringstream	iss(input);
+	std::string			token;
+	typedef std::vector<typename Container::value_type> vector_type;
+	typedef std::deque<typename Container::value_type> deque_type;
+	
+	if (typeid(result) != typeid(vector_type) and typeid(result) != typeid(deque_type))
+		throw std::invalid_argument ("Invalid container");
+	while (std::getline(iss, token, ' ')) {
+		size_t pos;
+		int	value = std::stoi(token, &pos);
+		if (pos != token.size()) {
+			throw std::invalid_argument("Invalid integer found in input");
+		}
+		if (value < 0) {
+			throw std::invalid_argument("Negative integer found in input");
+		}
+		result.push_back(value);
+	}
+	return result;
+}
+
+template<typename Container>
+Container	fillContainer(const std::string& input, int size) {
+	Container			result;
+	std::istringstream	iss(input);
+	std::string			token;
+
+	result.resize(size);
+
+	for (int i = 0; i < size && std::getline(iss, token, ' '); i++) {
+		size_t pos;
+		int	value = std::stoi(token, &pos);
+		if (pos != token.size()) {
+			throw std::invalid_argument("Invalid integer found in input");
+		}
+		if (value < 0) {
+			throw std::invalid_argument("Negative integer found in input");
+		}
+		result[i] = value;
+	}
+	return result;
+}
+
+template<typename Container>
+void	printContainer(const Container& output) {
+	for (size_t i = 0; i < output.size(); i++) {
+		std::cout << output[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 #endif
--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -1,18 +1,164 @@
 #include "PmergeMe.hpp"
 
-int	main(int argc, char **argv) {
-	std::string input;
+// **************************************************************************** //
+//								DEQUE										   	//
+// **************************************************************************** //
 
+int minRunLength(int n)
+{
+    int r = 0;      
+    while (n >= INSERT_THRESHOLD)
+    {
+        r |= (n & 1);
+        n >>= 1;
+    }
+    return n + r;
+}
+
+void insertionSort(std::deque<int>& d) {
+	for (int i = 1; i < (int) d.size(); i++) {
+		int key = d[i];
+        int j = i - 1;
+        while (j >= 0 && d[j] > key) {
+			d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = key;
+    }
+}
+
+void blockMergeSort(std::deque<int>& d, size_t blockSize) {
+    // Divide the deque into blocks
+    std::deque< std::deque<int> > blocks;
+    for (size_t i = 0; i < d.size(); i += blockSize) {
+		std::deque<int> block;
+		block.push_back(d[i]);
+        for (size_t j = i + 1; j < std::min(i + blockSize, d.size()); j++) {
+			block.push_back(d[j]);
+        }
+        insertionSort(block);
+        blocks.push_back(block);
+    }
+
+    while (blocks.size() > 1) {
+        std::deque<int> mergedBlock(blocks[0].size() + blocks[1].size());
+        size_t i = 0, j = 0, k = 0;
+        while (i < blocks[0].size() && j < blocks[1].size()) {
+            if (blocks[0][i] < blocks[1][j]) {
+                mergedBlock[k++] = blocks[0][i++];
+            } else {
+                mergedBlock[k++] = blocks[1][j++];
+            }
+        }
+        while (i < blocks[0].size()) {
+            mergedBlock[k++] = blocks[0][i++];
+        }
+        while (j < blocks[1].size()) {
+            mergedBlock[k++] = blocks[1][j++];
+        }
+		blocks.pop_front();
+		blocks.pop_front();
+        blocks.push_back(mergedBlock);
+    }
+
+    d = blocks.front();
+}
+
+// **************************************************************************** //
+//								VECTOR										   	//
+// **************************************************************************** //
+
+void	merge(std::vector<int> & left, std::vector<int> & right, std::vector<int> & result) {
+	size_t i = 0;
+	size_t j = 0;
+	size_t k = 0;
+
+	while (i < left.size() and j < right.size()) {
+		if (left[i] <= right[j]) {
+			result[k++] = left[i++];
+		} else {
+			result[k++] = right[j++];
+		}
+	}
+	while (i < left.size()) {
+		result[k++] = left[i++];
+	}
+	while (j < right.size()) {
+		result[k++] = right[j++];
+	}
+}
+
+void	insertion_sort(std::vector<int> &array, int start, int end) {
+	for (int i = start + 1; i <= end; i++) {
+		int tmp = array[i];
+		int j = i - 1; 
+		while (j >= start and array[j] > tmp) {
+			array[j + 1] = array[j];
+			j--;
+		}
+		array[j + 1] = tmp;
+	}
+}
+
+void	merge_insert_sort(std::vector<int> &array, int start, int end) {
+	if ( end - start < INSERT_THRESHOLD) {
+		insertion_sort(array, start, end);
+		return;
+	}
+	int mid = start + ((end - start) / 2);
+	merge_insert_sort(array, start, mid);
+	merge_insert_sort(array, mid + 1, end);
+	std::vector<int> left(array.begin() + start, array.begin() + mid + 1);
+	std::vector<int> right(array.begin() + mid + 1, array.begin() + end + 1);
+	std::vector<int> result(end - start + 1);
+	merge(left, right, result);
+	for (int i = start; i <= end; i++) {
+		array[i] = result[i - start];
+	}
+}
+
+// **************************************************************************** //
+//								MAIN										   	//
+// **************************************************************************** //
+
+int	main(int argc, char **argv) {
+	std::string 		input;
+	std::deque<int>		d;
+	clock_t				t_deque;
+	std::vector<int>	v;
+	clock_t				t_vector;
 
 	try {
+
 		if (argc == 1)
 			throw std::invalid_argument("No argument.");
+
+		int	n = argc - 1;
+
 		for (int i = 1; i < argc; i++) {
 			input.append(argv[i]);
 			input.append(" ");
 		}
-		std::vector c_vector(fillVector())
-		std::cout << input << std::endl;
+
+		t_deque = clock();
+		d = fillContainer<std::deque<int> >(input, n);
+		int minRun = minRunLength(n);
+		blockMergeSort(d, minRun);
+		t_deque = clock() - t_deque;
+
+		t_vector = clock();
+		v = fillContainer<std::vector<int> >(input, n);
+		merge_insert_sort(v, 0, v.size() - 1);
+		t_vector = clock() - t_vector;
+
+		std::cout << "Before:\n\t" << input << std::endl;
+		std::cout << "After:\n\tDEQUE\n\t\t";
+		printContainer<std::deque<int> >(d);
+		std::cout << "\tVECTOR\n\t\t";
+		printContainer<std::vector<int> >(v);
+
+		std::cout << "Time to process " << n << " elements with std::deque(" << INSERT_THRESHOLD <<"): " << (double)(t_deque) / CLOCKS_PER_SEC * 100000 << "us" << std::endl;
+		std::cout << "Time to process " << n << " elements with std::vector: " << (double)(t_vector) / CLOCKS_PER_SEC * 100000 << "us" << std::endl;
 	} catch(std::runtime_error const &e) {
 		std::cout << "FAILURE\t" << e.what() << std::endl;
 	} catch(std::exception const &e) {
